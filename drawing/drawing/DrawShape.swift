9e7b70ca2332af40ca781547c9b14381132ec40c
Richard <>
2021-03-05 23:35:15 -0500

    made it so that you can click on the screen to make the square follow you, it keeps track of the time it takes to lose

--- drawing/drawing/DrawShape.swift
+++ drawing/drawing/DrawShape.swift
@@ -9,53 +9,84 @@ import UIKit
 
 class DrawShape: UIView {
     
-    var leftEdge:Int = 0
-    var rightEdge:Int = Int(UIScreen.main.bounds.size.width)
-    var topEdge:Int = 0
-    var bottomEdge:Int = Int(UIScreen.main.bounds.size.height)
-
-    var drawX = 0
-    var drawY = 0
+    @IBOutlet var gameOverLabel:UILabel!
+    @IBOutlet var timeLastedLabel:UILabel!
+    @IBOutlet var YouLastedLabel:UILabel!
+    @IBOutlet var time:UILabel!
+    @IBOutlet var playAgainButton:UIButton!
     
-    var dx = 2
-    var dy = 2
+    
+    let leftEdge:Int = 0
+    let rightEdge:Int = Int(UIScreen.main.bounds.size.width)
+    let topEdge:Int = 0
+    let bottomEdge:Int = Int(UIScreen.main.bounds.size.height)
+
+    var drawX:Double = 0
+    var drawY:Double = 0
+    
+    var gotoX:Double = 0
+    var gotoY:Double = 0
+    
+    var dx:Double = 2
+    var dy:Double = 2
+    
+    var speed:Double = 5
+    var lastTouch:CGPoint = CGPoint(x:-50, y:-50)
+    var playingGame:Bool = true
+    
     
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         let color:UIColor = UIColor.blue
-        let drect = CGRect(x:drawX, y:drawY, width:40, height:60)
+        let drect = CGRect(x:drawX, y:drawY, width:40, height:40)
         let bpath:UIBezierPath = UIBezierPath(rect: drect)
         
         color.set()
         bpath.stroke()
-        UIColor.blue.set()
-        UIRectFill(drect)
-        print("DrawRect called")
-        
+        bpath.fill()
     }
+
+    
     @objc func update(){
-        print("DrawShape update")
-        drawX += dx
-        drawY += dy
-        
-        if(drawX >= rightEdge || drawX <= leftEdge){
-            drawX = -drawX
+        if(playingGame){
+            if(drawX > gotoX){
+                drawX += -dx
+            }
+            else if (drawX < gotoX){
+                drawX += dx
+            }
+            if(drawY > gotoY){
+                drawY += -dy
+            }
+            
+            else if(drawY < gotoY){
+                drawY += dy
+            }
+            
+            if(abs(Double(lastTouch.x) - drawX + Double(lastTouch.y) - drawY) <= 1){
+                gameOver()
+            }
+            print(lastTouch)
+            setNeedsDisplay()
         }
-        if(drawY >= bottomEdge || drawY <= topEdge){
-            drawY = -drawY
-        }
-        
-        
-        
-        setNeedsDisplay()
     }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches{
             let point = t.location(in: self)
             
-            drawX = Int(point.x)
-            drawY = Int(point.y)
+            gotoX = Double(point.x)
+            gotoY = Double(point.y)
+            
+            let width = drawX - gotoX
+            let length = drawY - gotoY
+
+            let hypotenuse:Double = (length * length + width * width).squareRoot()
+            dx = abs(drawX - gotoX)/(hypotenuse / speed)
+            dy = abs(drawY - gotoY)/(hypotenuse / speed)
+            
+            lastTouch = point
         }
     }
     
@@ -63,19 +94,36 @@ class DrawShape: UIView {
         for t in touches{
             let point = t.location(in: self)
             
-            drawX = Int(point.x)
-            drawY = Int(point.y)
-        }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches{
-            let point = t.location(in: self)
+            gotoX = Double(point.x)
+            gotoY = Double(point.y)
             
-            drawX = Int(point.x)
-            drawY = Int(point.y)
+            let width = drawX - gotoX
+            let length = drawY - gotoY
+
+            let hypotenuse:Double = (length * length + width * width).squareRoot()
+            dx = abs(drawX - gotoX)/(hypotenuse / speed)
+            dy = abs(drawY - gotoY)/(hypotenuse / speed)
+            
+            lastTouch = point
         }
     }
     
-
+    func gameOver(){
+        playingGame = false
+        gameOverLabel.isHidden = false
+        timeLastedLabel.isHidden = false
+        YouLastedLabel.isHidden = false
+        playAgainButton.isHidden = false
+        timeLastedLabel.text = time.text! + " seconds!"
+    }
+    
+    func restartGame(){
+        playingGame = true
+        gameOverLabel.isHidden = true
+        timeLastedLabel.isHidden = true
+        YouLastedLabel.isHidden = true
+        playAgainButton.isHidden = true
+        time.text = "0"
+        lastTouch = CGPoint(x:-50, y:-50)
+    }
 }
--- drawing/drawing/ViewController.swift
+++ drawing/drawing/ViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var time : UILabel!
+    
+    var numTime = 0
 
     @IBOutlet weak var drawShape : DrawShape!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,11 +22,31 @@ class ViewController: UIViewController {
         
         let displayLink = CADisplayLink(target: self, selector: #selector(update))
         displayLink.add(to: RunLoop.main, forMode: RunLoop.Mode.default)
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(changeTimer), userInfo: nil, repeats: true)
     }
 
     @objc func update(){
-        print("update")
         drawShape.update()
     }
+    
+    
+    @objc func changeTimer() {
+        numTime += 1
+        
+        if(numTime % 5 == 0){
+            drawShape.speed += 1
+        }
+        if(drawShape.playingGame){
+            time.text = String(numTime + 1)
+        }
+    }
+    
+    @IBAction func restartGame(){
+        numTime = 0
+        time.text = "0"
+        drawShape.restartGame()
+    }
+
 }
 
